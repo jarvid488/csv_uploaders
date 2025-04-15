d5639c088322b8cae84c7c4ede50da76574d43d5
jarvid488 <32535216+jarvid488@users.noreply.github.com>
2025-04-15 11:40:59 +0530

    Update streamlit_app.py
    Describing connection details in the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ from snowflake.snowpark.context import get_active_session
 st.set_page_config(page_title='csv file reader',layout='wide')
 st.title(f"Example Streamlit App :balloon: {st.__version__}")
 st.header('single fike upload')
+cnx=st.connection("snowflake")
+session = cnx.session()
 upload_file=st.file_uploader('Upload your CSV-file')
 df=pd.read_csv(upload_file)
 st.dataframe(df,width=1800, height=1200)
